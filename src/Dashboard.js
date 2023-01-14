14997aa20095725182207ae8056966f5e2787924
feyz <faizashrafkk@gmail.com>
2023-01-14 15:59:22 +0530

    Event List and server setup

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,7 +1,20 @@
+import EventsList from "./EventsList";
+import useFetch from "./Hook/useFetch";
+
 const Dashboard = () => {
+  const {
+    data: events,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/events");
+
   return (
     <div className="dashHome">
-      <h1>Hello WOrld</h1>
+      <div className="home">
+        {error && <div>{error}</div>}
+        {isPending && <div className="loading">Loading....</div>}
+        {events && <EventsList events={events} title="Event List" />}
+      </div>
     </div>
   );
 };
