ce4d5036c2dc98c0bdd0198327f4691903c14e2c
feyz <faizashrafkk@gmail.com>
2023-01-14 19:22:31 +0530

    added route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Dashboard from "./Dashboard";
 import Navbar from "./Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Dashboard />
+      <Router>
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            {/* <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/signup" element={<SignUp />} /> */}
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
